c8d7d8ddcb6f834e5b541b90ca5a35538704890b
Ben <bpinnegar04@gmail.com>
2024-05-22 13:50:35 +0100

    Added  initial function for creating the main window

---
+++ MainWindow.py
@@ -0,0 +1,20 @@
+from PyQt6.QtWidgets import QApplication, QWidget
+
+class MainWindow(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # set the window title
+        self.setWindowTitle('DBDL Perk Checker')
+
+        # show the window
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    # create the main window
+    window = MainWindow()
+
+    # start the event loop
+    app.exec()
