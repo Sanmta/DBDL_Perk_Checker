59c2fc7df793803355701a1a57cd76389a03c915
Ben <bpinnegar04@gmail.com>
2024-05-22 14:19:06 +0100

    Created new labels and methods for creating them

--- MainWindow.py
+++ MainWindow.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt6.QtGui import QFont
 
 class MainWindow(QWidget):
     def __init__(self, *args, **kwargs):
@@ -7,8 +9,23 @@ class MainWindow(QWidget):
         # set the window title
         self.setWindowTitle('DBDL Perk Checker')
 
+        # set the window size
+        self.setGeometry(100, 100, 1300, 800)
+
+        # create a label
+        label = create_label(self, 'Hello World!')
+        label.setFont(QFont('Roboto', 200))
+
+        # create a layout
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        self.setLayout(layout)
+
         # show the window
         self.show()
+
+def create_label(self, text):
+    return QLabel(text, self)
 
 if __name__ == '__main__':
     app = QApplication([])
